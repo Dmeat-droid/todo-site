2538b8181a28c47ca3f2d5df9b421dbafcad7462
Darwisy Ahmad A. <darwisyahmad54@gmail.com>
2026-08-11 21:28:01 +0700

    feat: add line-through and delete button. Also refactored the code and separate js and css files

---
+++ app.js
@@ -0,0 +1,71 @@
+"use strict";
+
+// Access element in html
+const form = document.getElementById("todo-form");
+const textInput = document.getElementById("todo-input");
+const list = document.getElementById("list-container");
+    
+let todos = JSON.parse(localStorage.getItem("todos") || "[]");
+if(!todos.length) todos = [{ id: 1, text: "Learn Javascript", completed: false}];
+
+//render list
+function renderTodos() {
+  list.innerHTML = "";
+
+  todos.forEach(element => {
+    const li = document.createElement("li");
+
+    // checkbox
+    const cb = document.createElement("input");
+    cb.type = "checkbox";
+    cb.checked = element.completed;
+    cb.addEventListener("change", () => {
+      element.completed = cb.checked;
+      saveAndRender();
+    });
+
+    // Text label
+    const span = document.createElement("span");
+    span.textContent = " " + element.text;
+    if (element.completed) {
+      span.style.textDecoration = "line-through";
+    }
+
+    // Delete Button
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.addEventListener("click", () => {
+      todos = todos.filter((item) => item.id !== element.id);
+      saveAndRender();
+    });
+
+    li.appendChild(cb);
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+    // li.appendChild(document.createTextNode(" " + element.text));
+    list.appendChild(li);
+  });
+}
+
+function saveAndRender() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+  renderTodos();
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const text = textInput.value.trim();
+  if (!text) {
+    console.log("Input value is null!");
+    return;
+  }
+  todos.push({ id: Date.now(), text, completed: false });
+  textInput.value = "";
+  saveAndRender();
+});
+
+// disable submit when its empty
+textInput.addEventListener("input", () => {
+  const submitBtn = document.getElementById("todo-submit");
+  if (submitBtn) submitBtn.disabled = !textInput.value.trim();
+});
